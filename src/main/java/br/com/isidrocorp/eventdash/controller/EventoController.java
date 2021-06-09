88d2027ff6f117f3fb2c961f2af5a744a69f0487
professorisidro <isidro@professorisidro.com.br>
2021-06-09 17:09:28 -0300

    recuperar eventos por periodo

--- src/main/java/br/com/isidrocorp/eventdash/controller/EventoController.java
+++ src/main/java/br/com/isidrocorp/eventdash/controller/EventoController.java
@@ -1,10 +1,12 @@
 package br.com.isidrocorp.eventdash.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.isidrocorp.eventdash.dao.EventoDAO;
@@ -22,6 +24,14 @@ public class EventoController {
 		ArrayList<Evento> lista;
 		lista = (ArrayList<Evento>)dao.findAll();
 		return lista;
+	}
+	
+	@GetMapping("/eventosporperiodo")
+	public ArrayList<Evento> recuperarPorPeriodo(@RequestParam(name="inicio")String inicio,
+			                                     @RequestParam(name="fim") String fim){
+		LocalDate dataIni = LocalDate.parse(inicio);
+		LocalDate dataFim = LocalDate.parse(fim);
+		return dao.findAllByDataBetween(dataIni,dataFim);
 	}
 
 }
