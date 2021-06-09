b33516d42215cdf876993053e99e4b237fe10c8e
professorisidro <isidro@professorisidro.com.br>
2021-06-09 16:31:18 -0300

    evento controller

--- src/main/java/br/com/isidrocorp/eventdash/controller/EventoController.java
+++ src/main/java/br/com/isidrocorp/eventdash/controller/EventoController.java
@@ -1,5 +1,27 @@
 package br.com.isidrocorp.eventdash.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.isidrocorp.eventdash.dao.EventoDAO;
+import br.com.isidrocorp.eventdash.model.Evento;
+
+@RestController
+@CrossOrigin("*")
 public class EventoController {
+	
+	@Autowired
+	private EventoDAO dao;
+	
+	@GetMapping("/eventos")
+	public ArrayList<Evento> recuperarTodos(){
+		ArrayList<Evento> lista;
+		lista = (ArrayList<Evento>)dao.findAll();
+		return lista;
+	}
 
 }
