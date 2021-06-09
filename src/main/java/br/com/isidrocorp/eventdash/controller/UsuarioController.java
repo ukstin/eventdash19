4fa210c4d5e0aa3ce397b77cfd15201faf005a98
professorisidro <isidro@professorisidro.com.br>
2021-06-09 09:05:09 -0300

    primeiro commit

---
+++ src/main/java/br/com/isidrocorp/eventdash/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package br.com.isidrocorp.eventdash.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.isidrocorp.eventdash.dao.UsuarioDAO;
+import br.com.isidrocorp.eventdash.model.Usuario;
+
+@RestController
+public class UsuarioController {
+	
+	// injeção da dependência do DAO no meu controller
+	@Autowired
+	private UsuarioDAO dao;
+	
+	@GetMapping("/usuarios")
+	public ArrayList<Usuario> recuperarTodos(){
+		ArrayList<Usuario> lista;
+		lista = (ArrayList<Usuario>)dao.findAll();
+		return lista;
+		
+	}
+	
+
+}
